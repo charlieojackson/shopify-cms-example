13367333623ec6ff6e35ffdcd6df29a47ab52aba
Charlie Jackson <32334954+charlieojackson@users.noreply.github.com>
2023-11-30 14:46:49 +0000

    test

--- pages/1_Product_Tags.py
+++ pages/1_Product_Tags.py
@@ -13,23 +13,31 @@ dct = {
     'sofa': {
         'colours': ['red', 'blue', 'green'],
         'number_of_seats': ['1 seat', '2 seater', '3 seater'],
+        'main_material': ['leather', 'fabric', 'velvet', 'wood'],
+        'back_style': ['1 seat', '2 seater', '3 seater']
     },
     'table': {
         'wood_type': ['oak', 'pine', 'teak'],
         'table_top_material': ['glass', 'ceramic', 'wood'],
+        'main_material': ['leather', 'fabric', 'velvet', 'wood'],
+        'leg_material': ['wood', 'metal', 'plastic'],
     },
     'tv console': {
         'number_of_doors': ['0 doors', '1 door', '2 doors'],
         'number_of_drawers': ['0 drawers','1 draw', '2 drawers', '3 drawers','4 drawers'],
+        'style': ['industrial', 'modern', 'scandi'],
+    },
+    'rug': {
+        'pattern': ['striped', 'geometric'],
+        'pile_type': ['short', 'long'],
     }
 }
 
 
-product_type = st.selectbox('Select a product type', ('sofa', 'table', 'tv console'))
+product_type = st.selectbox('Select a product type', ('sofa', 'table', 'tv console', 'rug'))
 
 if product_type:
-    st.write(product_type)
     product_attributes = dct[product_type]
     for k, values in product_attributes.items():
         values = tuple(values)
-        attribute = st.multiselect(f'Choose a {k}', values)
+        attribute = st.multiselect(f'Choose {k} (multi select)', values)
