d143b211f185b30671e3c3afb07632fbd97876ba
Charlie Jackson <32334954+charlieojackson@users.noreply.github.com>
2023-11-30 14:40:23 +0000

    test

--- pages/1_Product_Tags.py
+++ pages/1_Product_Tags.py
@@ -12,20 +12,20 @@ from streamlit.hello.utils import show_code
 dct = {
     'sofa': {
         'colours': ['red', 'blue', 'green'],
-        'number_of_seats': ['red', 'blue', 'green'],
+        'number_of_seats': ['1 seat', '2 seater', '3 seater'],
     },
     'table': {
-        'colours': ['red', 'blue', 'green'],
+        'wood_type': ['oak', 'pine', 'teak'],
         'table_top_material': ['glass', 'ceramic', 'wood'],
     },
     'tv console': {
-        'colours': ['red', 'blue', 'green'],
-        'number_of_drawers': ['1 draw', '2 drawers', '3 drawers','4 drawers'],
+        'number_of_doors': ['0 doors', '1 door', '2 doors'],
+        'number_of_drawers': ['0 drawers','1 draw', '2 drawers', '3 drawers','4 drawers'],
     }
 }
 
 
-product_type = st.selectbox('Select a product type', ('sofa', 'table', 'tv console'))
+product_type = st.multiselect('Select a product type', ('sofa', 'table', 'tv console'))
 
 if product_type:
     product_attributes = dct[product_type]
